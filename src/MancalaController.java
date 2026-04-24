79175cad8f65b6fc98076912786563db5e1b115b
tones00 <anthony.moll22@gmail.com>
2026-04-24 11:33:54 -0700

    ManacalaController listeners and skeleton

--- src/MancalaController.java
+++ src/MancalaController.java
@@ -1,7 +1,47 @@
 public class MancalaController {
+
     private MancalaModel mancalaModel;
+    private MancalaView mancalaView;
 
-    public MancalaController() {
-
+    /**
+     * Creates controller and attaches listeners to all interactive components.
+     * @param model the game model
+     * @param view the game view
+     */
+    public MancalaController(MancalaModel model, MancalaView view) {
+        this.mancalaModel = model;
+        this.mancalaView = view;
+        attachPitListeners();
+        attachButtonListeners();
     }
+
+    /**
+     * Attaches this controller as a MouseListener to all pit panels.
+     */
+    private void attachPitListeners() {
+        Jpanel[] pits = mancalaView.getPits();
+        for (int i = 0; i< pits.length; i++) {
+            pits[i].addMouseListener(this);
+        }
+
+    /**
+     * Attaches this controller as an ActionListener to the undo button.
+     */
+    private void attachButtonListeners() {
+        view.getUndoButton().addActionListener(this);
+    }
+
+    // remaining methods stay as TODO for now
+    public void pitClicked(int pitIndex) { // TODO }
+    public void undoClicked() { // TODO }
+    public void styleSelected(BoardStyle style) { // TODO }
+    public void stonesEntered(String input) { // TODO }
+    private void attachPitListeners() { // TODO }
+
+    @Override public void mouseClicked(MouseEvent e) { // TODO }
+    @Override public void mousePressed(MouseEvent e) {}
+    @Override public void mouseReleased(MouseEvent e) {}
+    @Override public void mouseEntered(MouseEvent e) {}
+    @Override public void mouseExited(MouseEvent e) {}
+    @Override public void actionPerformed(ActionEvent e) { // TODO }
 }
